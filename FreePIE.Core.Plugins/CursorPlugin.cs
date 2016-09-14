979e6be814bc7f811d915facf397640e9b4dfa23
Andrés Aparicio <adapar@gmail.com>
2016-09-14 01:18:39 -0300

    New cursor position plugin

---
+++ FreePIE.Core.Plugins/CursorPlugin.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using FreePIE.Core.Contracts;
+using FreePIE.Core.Plugins.Strategies;
+using SlimDX.DirectInput;
+using SlimDX.RawInput;
+
+namespace FreePIE.Core.Plugins
+{
+
+    [GlobalType(Type = typeof(CursorGlobal))]
+    public class CursorPlugin : Plugin
+    {
+        // Cursor position state variables
+        private int posXOut;
+        private int posYOut;
+
+        public override object CreateGlobal()
+        {
+            return new CursorGlobal(this);
+        }
+
+        public override Action Start()
+        {
+            IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
+
+            CursorPosIO.ClipCursor();
+
+            OnStarted(this, new EventArgs());
+            return null;
+        }
+
+        public override void Stop()
+        {
+
+        }
+        
+        public override string FriendlyName
+        {
+            get { return "Cursor"; }
+        }
+
+        public override void DoBeforeNextExecute()
+        {
+            // If a cursor command was given in the script, issue it all at once right here
+            if ((int)posXOut != 0 || (int)posYOut != 0)
+            {
+                CursorPosIO.SetCursorPos(posXOut, posYOut);
+
+                // Reset the cursor values
+                if (posXOut != 0)
+                {
+                    posXOut = 0;
+                }
+                if (posYOut != 0)
+                {
+                    posYOut = 0;
+                }
+            }
+        }
+
+        public int PosX
+        {
+            set
+            {
+                posXOut = posXOut + value;
+            }
+
+            get {
+                return (int)CursorPosition.x;
+            }
+        }
+
+        public int PosY
+        {
+            set
+            {
+                posYOut = posYOut + value;
+            }
+
+            get
+            {
+                return (int)CursorPosition.y;
+            }
+        }
+
+        private CursorPosIO.POINT CursorPosition
+        {
+            get
+            {
+                CursorPosIO.POINT cursorPos;
+                CursorPosIO.GetCursorPos(out cursorPos);
+                return cursorPos;
+            }
+        }
+    }
+
+    [Global(Name = "cursor")]
+    public class CursorGlobal
+    {
+        private readonly CursorPlugin plugin;
+
+        public CursorGlobal(CursorPlugin plugin)
+        {
+            this.plugin = plugin;
+        } 
+
+        public int posX
+        {
+            get { return plugin.PosX; }
+            set { plugin.PosX = value; }
+        }
+
+        public int posY
+        {
+            get { return plugin.PosY; }
+            set { plugin.PosY = value; }
+        }
+    }
+}
---
+++ FreePIE.Core.Plugins/CursorPosIO.cs
@@ -0,0 +1,36 @@
+﻿// The CursorPosIO class encapsulates the Win32 functions ClipCursor(),
+// SetCursorPos()
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace FreePIE.Core.Plugins
+{
+    class CursorPosIO
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public long left;
+            public long top;
+            public long right;
+            public long bottom;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public long x;
+            public long y;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint ClipCursor();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+    }
+}
